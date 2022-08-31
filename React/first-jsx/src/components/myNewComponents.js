a291aa14296baaf57f64e9b9cc3264af02c34e82
Alex Schaffer <aschaffer8770@gmail.com>
2022-08-30 18:47:53 -0700

    new components

---
+++ React/first-jsx/src/components/myNewComponents.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class MyNewComponent extends Component{
+    render(){
+        return(
+            <div>
+                We are now writting in MyNewComponent!
+            </div>
+        );
+    }
+}
+
+export default MyNewComponent;
