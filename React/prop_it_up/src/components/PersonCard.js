8ee9695423e33ce7da86e165ba210042a9adbb38
Alex Schaffer <aschaffer8770@gmail.com>
2022-09-01 16:59:36 -0700

    inversion

--- React/prop_it_up/src/components/PersonCard.js
+++ React/prop_it_up/src/components/PersonCard.js
@@ -1,16 +1,14 @@
 import React, { Component } from 'react'
 
-class PersonCard extends Component {
-    render (){
+const PersonCard = (props) => {
         return (
             <div>
-                <h1>{this.props.lastName}, {this.props.firstName}</h1>
-                <p>Age: {this.props.age}</p>
-                <p>Hair Color: {this.props.hairColor}</p>
+                <h1>{props.lastName}, {props.firstName}</h1>
+                <p>Age: {props.age}</p>
+                <p>Hair Color: {props.hairColor}</p>
                 <h1></h1>
             </div>
         );
     }
-}
 
 export default PersonCard
