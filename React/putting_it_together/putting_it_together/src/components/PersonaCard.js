a63450281ec81b4529e2d01b8774ea38f31a009b
Alex Schaffer <aschaffer8770@gmail.com>
2022-09-01 16:07:41 -0700

    setState for multi counter

--- React/putting_it_together/putting_it_together/src/components/PersonaCard.js
+++ React/putting_it_together/putting_it_together/src/components/PersonaCard.js
@@ -4,17 +4,20 @@ export class PersonaCard extends Component {
   constructor(props) {
     super(props)
     this.state = {
-        age : 45,
-        secondAge : 88
+        age : this.props.age
     }
   }
+  increaseAge = () => {
+    this.setState({ age: this.state.age + 1})
+  }
+
   render() {
     return (
       <div>
         <h1>{this.props.lastName}, {this.props.firstName}</h1>
         <p> Age: {this.state.age}</p>
         <p>Hair Color: {this.props.hairColor}</p>
-        <button onClick={() => this.setState({age: this.state.age +1})}>Birthday Button for {this.props.firstName} {this.props.lastName}</button>
+        <button onClick={this.increaseAge}>Birthday Button for {this.props.firstName} {this.props.lastName}</button>
       </div>
     )
   }
