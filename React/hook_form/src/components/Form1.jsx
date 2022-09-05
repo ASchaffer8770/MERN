5437e2cd4d79b029bfd62fb9c2db95b9d3eb14c1
Alex Schaffer <aschaffer8770@gmail.com>
2022-09-05 13:05:47 -0700

    added form validations

--- React/hook_form/src/components/Form1.jsx
+++ React/hook_form/src/components/Form1.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import DisplayForm from './DisplayForm'
+
 
 const Form1 = () => {
     const [firstName, setfirstName] = useState("")
@@ -7,34 +7,86 @@ const Form1 = () => {
     const [email, setemail] = useState("")
     const [password, setpassword] = useState("")
     const [confirmPassword, setconfirmPassword] = useState("")
+    const [hasBeenSubmitted, setHasBeenSubmitted] = useState(false)
+
+    const createUser = (e) => {
+        e.preventDefault()
+        // LINE 15 IS FORM VALIDATION
+        if(firstName.length > 3 && lastName.length > 3 && email.length > 5 && password.length === confirmPassword.length) {
+        const newUser = {firstName: firstName, lastName: lastName, email: email, password: password, confirmPassword: confirmPassword}
+        console.log("Created", newUser) //sends info to backend instead of console logging
+        setHasBeenSubmitted(true) //sets state to true after beign submitted
+        } else {
+            alert("Invalid Form")
+        }
+        
+    }
+
+    const formMessage = (e) => {
+        if (hasBeenSubmitted) {
+            return "Thank you! Form Has Been Submitted"
+        } else {
+            return "Please Enter Your Information"
+            }
+        
+    }
 
     return (
         <div>
-             <h1>Hook Form</h1>
-                <form>
+             <h1>{formMessage ()}</h1>
+                <form onSubmit={ createUser }>
                     <div>
                         <label>First Name: </label> 
                         <input type="text" name='firstName' value= {firstName} onChange={(e) => setfirstName(e.target.value)} />
+                    </div>
+                    <div>
+                        {
+                            firstName.length < 3 && //Ternary Operator to create validation on the form
+                            <span style = {{color: "red"}}>First Name must be more than 2 Characters</span>
+                        }
                     </div>
                     <div>
                         <label>Last Name: </label> 
                         <input type="text" name='lastName' value={lastName} onChange={(e) => setlastName(e.target.value)} />
                     </div>
                     <div>
+                        {
+                            lastName.length < 3 && //Ternary Operator to create validation on the form
+                            <span style = {{color: "red"}}>First Name must be more than 2 Characters</span>
+                        }
+                    </div>
+                    <div>
                         <label>Email: </label>
                         <input type="text" name ='email' value={email} onChange = {(e)=> setemail(e.target.value)}/>
+                    </div>
+                    <div>
+                        {
+                            email.length < 6 && //Ternary Operator to create validation on the form
+                            <span style = {{color: "red"}}>Email must be more than 5 Characters</span>
+                        }
                     </div>
                     <div>
                         <label>Password: </label>
                         <input type="password" name='password' value={password} onChange = {(e) => setpassword(e.target.value)} />
                     </div>
                     <div>
+                        {
+                            password.length < 8 && //Ternary Operator to create validation on the form
+                            <span style = {{color: "red"}}>Password must be more than 8 Characters</span>
+                        }
+                    </div>
+                    <div>
                         <label>Confirm Password: </label>
                         <input type="password" name='confirmPassword' value={confirmPassword} onChange = {(e) => setconfirmPassword(e.target.value)} />
                     </div>
-                    <input type="submit" value="Submit" />
+                    <div>
+                        {
+                            confirmPassword.length < 8 && //Ternary Operator to create validation on the form
+                            <span style = {{color: "red"}}>Passwords must match</span>
+                        }
+                    </div>
+                    <input type="submit" value="Create User" />
                 </form>
-                <DisplayForm firstName = {firstName} lastName = {lastName} email = {email} password = {password} confirmPassword = {confirmPassword}/>
         </div>
   )
 }
