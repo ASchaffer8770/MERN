466d055e7f064c0bfc9b6d7af3ef5da9d0a913d3
Alex Schaffer <aschaffer8770@gmail.com>
2022-09-07 15:16:30 -0700

    added routing

--- React/luke_api/src/App.js
+++ React/luke_api/src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import {Routes, Route} from 'react-router-dom';
+
+const Home = () => {
+  return (
+    <div>
+        <h2>Home.jsx</h2>
+    </div>
+  )
+}
+
+const About = () => {
+  return (
+    <div>
+        <h2>About.jsx </h2>
+    </div>
+  )
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Star Wars API</h1>
+      <h2>Header can go here</h2>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/about" element={<About/>}/>
+      </Routes>
     </div>
   );
 }
