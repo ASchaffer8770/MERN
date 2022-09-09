35c93109d7b6fa4a80ea3ee9b519aa1ba7fc61e7
Alex Schaffer <aschaffer8770@gmail.com>
2022-09-08 18:29:25 -0700

    init comit

---
+++ Express/Faker_API/server/server.js
@@ -0,0 +1,15 @@
+//1 IMPORT DEPENDENCIES
+const express = require("express")
+const app = express()
+
+//2 CONFIGFURE EXPRESS
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+
+//3 ROUTES
+
+
+
+
+//OPEN PORTS
