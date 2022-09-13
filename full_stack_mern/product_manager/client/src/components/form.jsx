7c716e8931b9440a9c88ddfbe655c60263f5f7ef
Alex Schaffer <aschaffer8770@gmail.com>
2022-09-13 12:02:32 -0700

    updates to backend

---
+++ full_stack_mern/product_manager/client/src/components/form.jsx
@@ -0,0 +1,11 @@
+import React, { useEffect, useState} from 'react'
+import axios from 'axios'
+
+
+const form = () => {
+  return (
+    <div>form</div>
+  )
+}
+
+export default form
--- full_stack_mern/product_manager/server.js
+++ full_stack_mern/product_manager/server.js
@@ -2,6 +2,8 @@ const express = require("express")
 
 const app = express()
 
+require('./server/routes/person.routes')(app)
+
 app.listen(8000, () => {
     console.log(`Listening on Port: 8000`)
 })
