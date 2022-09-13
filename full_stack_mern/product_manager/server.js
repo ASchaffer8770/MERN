5892879be51ee351ed20ffe47d0075b606267d4d
Alex Schaffer <aschaffer8770@gmail.com>
2022-09-13 14:48:43 -0700

    reset

--- full_stack_mern/product_manager/server.js
+++
@@ -1,9 +0,0 @@
-const express = require("express")
-
-const app = express()
-
-require('./server/routes/person.routes')(app)
-
-app.listen(8000, () => {
-    console.log(`Listening on Port: 8000`)
-})
--- full_stack_mern/product_manager/server/routes/person.routes.js
+++
@@ -1,5 +0,0 @@
-const personController = require("../controllers/person.controller")
-
-module.exports = function(app) {
-    app.get('/api', personController.index)
-}
